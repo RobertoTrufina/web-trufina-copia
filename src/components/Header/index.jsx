a64ae08f1be00843a48e188a3d3d9e5bc8c6781f
Roberto Pinto <robertotrufina@gmail.com>
2023-02-22 23:42:45 -0300

    inicio carrossel

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import './style.css';
+
+import { BsCart3 } from 'react-icons/bs';
+import { HiOutlineMagnifyingGlass } from 'react-icons/hi2';
+
+import logoTrufina from '../../imgs/logoTrufina.png';
+
+export default function Header() {
+    return (
+        <div>
+            <header className="header-content">
+                <img src={logoTrufina} />
+
+                <div className="header-navBar-search-cart">
+
+                    <nav className="header-content-menu">
+                        <ul>
+                            <li className="li-home"><a href="#">Home</a></li>
+                            <li className="li-products"><a href="#">Nossos Produtos</a>
+                                <ul>
+                                    <li><a href="#">Trufas</a></li>
+                                    <li><a href="#">Geleias</a></li>
+                                    <li><a href="#">Docinhos</a></li>
+                                    <li><a href="#">Brownies</a></li>
+                                    <li><a href="#">Tortas</a></li>
+                                    <li><a href="#">Bolos</a></li>
+                                    <li><a href="#">Bem-casado</a></li>
+                                </ul>
+                            </li>
+                            <li className="li-contact"><a href="#">Fale Conosco</a></li>
+                        </ul>
+                    </nav>
+
+                    <div className="input-input-search-btn">
+                        <div className="header-input-search">
+                            <input type="text" placeholder="Buscar Produto"
+                                autoComplete="on" />
+                            <button type="submit" className="header-input-btn" title="Pesquisar">
+                                <HiOutlineMagnifyingGlass color="#928282" size={18} />
+                            </button>
+                        </div>
+                    </div>
+                    <BsCart3 color="#fff" size={25} title="Compras" />
+                </div>
+            </header>
+
+
+        </div>
+
+
+    )
+
+
+}
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,54 +1,27 @@
 import React from "react";
+import Header from "../../components/header";
 
-import { BsCart3 } from 'react-icons/bs';
-import { HiOutlineMagnifyingGlass } from 'react-icons/hi2';
-
-import logoTrufina from '../../imgs/logoTrufina.png';
+import img1Carrosel from '../../imgs/img2-carrosel.png';
 
 import './style.css';
-
 
 function Home() {
 
   return (
+
     <div className="home-container">
 
-      <header className="header-content">
-        <img src={logoTrufina} />
+      <Header />
 
-        <div className="header-navBar-search-cart">
-
-          <nav className="header-content-menu">
-            <ul>
-              <li className="li-home"><a href="#">Home</a></li>
-              <li className="li-products"><a href="#">Nossos Produtos</a>
-                <ul>
-                  <li><a href="#">Trufas</a></li>
-                  <li><a href="#">Geleias</a></li>
-                  <li><a href="#">Docinhos</a></li>
-                  <li><a href="#">Brownies</a></li>
-                  <li><a href="#">Tortas</a></li>
-                  <li><a href="#">Bolos</a></li>
-                  <li><a href="#">Bem-casado</a></li>
-                </ul>
-              </li>
-              <li className="li-contact"><a href="#">Fale Conosco</a></li>
-            </ul>
-          </nav>
-
-          <div className="input-input-search-btn">
-            <div className="header-input-search">
-              <input type="text" placeholder="Buscar Produto"
-                autoComplete="on" />
-              <button type="submit" className="header-input-btn" title="Pesquisar">
-                <HiOutlineMagnifyingGlass color="#928282" size={18} />
-              </button>
-            </div>
-          </div>
-          <BsCart3 color="#fff" size={25} title="Compras" />
+      <main>
+        
+        <div className="carousel">
+          <img src={img1Carrosel} />
+          <p className="text1-carousel">Aprecie o que há <br /> de melhor em <br /> nossos recheios</p>
+          <p className="text2-carousel">Perfeitas para presente, <br /> festas, sobremesas e em <br /> qualquer ocasião.</p>
         </div>
 
-      </header>
+      </main>
     </div>
   )
 }
